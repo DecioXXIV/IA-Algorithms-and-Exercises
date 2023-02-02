910a919747420cb77bc62829440a92c319b190d4
Riccardo De Cesaris <ric.dc@hotmail.it>
2023-02-02 14:09:29 +0100

    Update graph_blind_search_to_obj.py

--- graph_blind_search/graph_blind_search_to_obj.py
+++ graph_blind_search/graph_blind_search_to_obj.py
@@ -2,17 +2,19 @@ import numpy as np
 
 class Node:
     pass
-    def __init__(self, state, parent, cost_from_parent):
+    def __init__(self, state, parent, cost_from_parent, g):
         self.state = state
         self.parent = parent
         self.cost_from_parent = cost_from_parent
+        self.cost_so_far = g
     
     def solution_dump(self, iteration):
         solution_path = list()
-        total_cost = 0
+        #total_cost = 0
+        total_cost = self.cost_so_far
         while self is not None:
             solution_path.append(self.state)
-            total_cost += self.cost_from_parent
+            #total_cost += self.cost_from_parent
             self = self.parent
         solution_path.reverse()
         print("*** FINAL ITERATION: %d" % iteration) 
@@ -83,7 +85,7 @@ class Graph:
         iteration = 1
         while iterate == True:
             if iteration == 1:
-                current_node = Node(state=starting_node, parent=None, cost_from_parent=0)
+                current_node = Node(state=starting_node, parent=None, cost_from_parent=0, g=0)
             else:
                 current_node = fringe.pop(0)
             
@@ -115,7 +117,8 @@ class Graph:
                     neighbor_idx = self.graph_nodes.index(neighbor)
                     cost_from_parent = self.graph_arcs[current_node_idx][neighbor_idx]
 
-                    new_neighbor = Node(state=self.graph_nodes[neighbor_idx], parent=current_node, cost_from_parent=cost_from_parent)
+                    new_neighbor = Node(state=self.graph_nodes[neighbor_idx], parent=current_node, cost_from_parent=cost_from_parent, g=0)
+                    new_neighbor.cost_so_far = new_neighbor.parent.cost_so_far + new_neighbor.cost_from_parent
                     fringe.append(new_neighbor)
             
 # The Algorithm ends when it founds the Solution or if the Fringe is empty.
@@ -168,7 +171,7 @@ class Graph:
         iteration = 1
         while iterate == True:
             if iteration == 1:
-                current_node = Node(state=starting_node, parent=None, cost_from_parent=0)
+                current_node = Node(state=starting_node, parent=None, cost_from_parent=0, g=0)
             else:
                 current_node = fringe.pop(0)
             
@@ -200,7 +203,8 @@ class Graph:
                     neighbor_idx = self.graph_nodes.index(neighbor)
                     cost_from_parent = self.graph_arcs[current_node_idx][neighbor_idx]
 
-                    new_neighbor = Node(state=neighbor, parent=current_node, cost_from_parent=cost_from_parent)
+                    new_neighbor = Node(state=neighbor, parent=current_node, cost_from_parent=cost_from_parent, g=0)
+                    new_neighbor.cost_so_far = new_neighbor.parent.cost_so_far + new_neighbor.cost_from_parent
                     fringe.insert(0,new_neighbor)
             
 # The Algorithm ends when it founds the Solution or if the Fringe is empty.
@@ -209,6 +213,100 @@ class Graph:
                 print(node.state + " ", end="")
             print("]")
             
+            if len(fringe) == 0:
+                iterate = False
+                print("*** EMPTY FRINGE! END OF THE SEARCH ***\n")
+                break
+
+            iteration += 1
+            print()
+
+        if solution_node is not None:
+            solution_node.solution_dump(iteration)
+        else:
+            print("*** THERE IS NOT AN OBJECTIVE NODE IN THE GRAPH! ***\n")
+
+# *** ******************* ***
+# *** UNIFORM COST SEARCH ***
+# *** ******************* ***
+
+    def uniform_cost_search_to_obj(self):
+        print("***************************************")
+        print("*** UNIFORM COST SEARCH, GRAPH MODE ***")
+        print("***************************************\n")
+        starting_node = None
+
+# The Search begins from a Starting Node, which must be in the Graph
+        while(starting_node is None):
+            print("Starting Node: ", end="")
+            node = input()
+            if node in self.graph_nodes:
+                starting_node = node
+            else:
+                print("This Node is not in the Graph!\n")
+
+# ALGORITHM'S EXPLANATION: this version exploits the "Graph Search" model, in which the Fringe contains only the "not-yet-expanded" Nodes.        
+        print("\n*** STARTING THE SEARCH... ***")
+        closed_list = list()
+        fringe = list()
+
+        solution_node = None
+
+# STEP 1: We decide to expand the first Node in the Fringe, which gets removed from it and gets appended to the Closed List.
+        iterate = True
+        iteration = 1
+        while iterate == True:
+            if iteration == 1:
+                current_node = Node(state=starting_node, parent=None, cost_from_parent=0, g=0)
+            else:
+                current_node = fringe.pop(0)
+            
+            print("ITERATION: %d" % iteration)
+            print("Expanded Node: %s" % current_node.state)
+
+            if current_node.state not in closed_list:
+                closed_list.append(current_node.state)
+
+# STEP 2: For the "current_node" we execute the Goal Test: if it succeeds we have found the Solution and the Algorithm ends, otherwise we retrieve all the Neighbors.
+            current_node_idx = self.graph_nodes.index(current_node.state)
+
+            if self.objectives[current_node_idx] == 1:
+                iterate = False
+                solution_node = current_node
+                print("*** SOLUTION FOUND! END OF THE SEARCH ***\n")
+                break
+
+            current_node_neighbors = list()
+            for i in range(len(self.graph_arcs[current_node_idx])):
+                if self.graph_arcs[current_node_idx][i] != 0:
+                    current_node_neighbors.append(self.graph_nodes[i])
+
+# STEP 3: The Neighbors of the "current_node" are added to the Fringe, but only if they are "not-yet-expanded" Nodes.
+# Insight: in this version of the Algorithm, the Fringe is ordered by increasing total cost (root to leaf)
+            for neighbor in current_node_neighbors:
+                if neighbor not in closed_list:
+                    neighbor_idx = self.graph_nodes.index(neighbor)
+                    cost_from_parent = self.graph_arcs[current_node_idx][neighbor_idx]
+
+                    new_neighbor = Node(state=neighbor, parent=current_node, cost_from_parent=cost_from_parent, g=0)
+                    new_neighbor.cost_so_far = new_neighbor.parent.cost_so_far + new_neighbor.cost_from_parent
+
+                    if len(fringe) == 0:
+                        fringe.append(new_neighbor)
+                    else:
+                        for fringe_idx in range(len(fringe)):
+                            if fringe[fringe_idx].cost_so_far > new_neighbor.cost_so_far:
+                                fringe.insert(fringe_idx, new_neighbor)
+                                break
+                    if new_neighbor not in fringe:
+                        fringe.append(new_neighbor)
+
+# The Algorithm ends when it founds the Solution or if the Fringe is empty.            
+            print("Fringe: [ ", end="")
+            for node in fringe:
+                print(node.state + " ", end="")
+            print("]")
+
             if len(fringe) == 0:
                 iterate = False
                 print("*** EMPTY FRINGE! END OF THE SEARCH ***\n")
@@ -236,3 +334,23 @@ class Graph:
 
 # graph.breadth_search_to_obj()
 # graph.depth_search_to_obj()
+
+graph_infos = dict()
+graph_infos['AQ'] = [('RM',11),('AN',19),('PG',17)]
+graph_infos['AN'] = [('BO',21),('AQ',19),('BA',46),('PG',16)]
+graph_infos['BA'] = [('RM',46),('AN',45)]
+graph_infos['BO'] = [('AN',21),('MI',21),('FI',10)]
+graph_infos['FI'] = [('BO',10),('RM',28),('PG',15),('PI',9),('GE',22)]
+graph_infos['GE'] = [('FI',22),('PI',16),('MI',14)]
+graph_infos['MI'] = [('TO',14),('GE',14),('BO',21)]
+graph_infos['NA'] = [('RM',22)]
+graph_infos['PG'] = [('RM',17),('FI',15),('AN',16),('AQ',17)]
+graph_infos['PI'] = [('FI',9),('GE',16),('RM',37)]
+graph_infos['RM'] = [('NA',22),('AQ',11),('BA',45),('PI',17),('PG',37),('FI',28)]
+graph_infos['TO'] = [('MI',14)]
+
+objectives = [0,0,0,0,0,0,0,1,0,0,0,0] # NA is the default Objective Node
+
+graph = Graph(graph_infos, objectives)
+
+graph.uniform_cost_search_to_obj()
